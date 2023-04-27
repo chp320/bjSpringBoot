3699778b2ace292457ed44263d1e10fc34650d2a
chp320 <chp320@naver.com>
2023-04-27 23:40:54 +0900

    스프링에서 제공하는 API를 사용해서 '유효성 검증' 수행

--- src/main/java/com/study/helloworld/ContentValidator.java
+++ src/main/java/com/study/helloworld/ContentValidator.java
@@ -1,6 +1,7 @@
 package com.study.helloworld;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class ContentValidator implements Validator {
@@ -14,16 +15,24 @@ public class ContentValidator implements Validator {
     public void validate(Object obj, Errors errors) {
         ContentDto dto = (ContentDto) obj;
 
+//        String sWriter = dto.getWriter();
+//        if (sWriter == null || sWriter.trim().isEmpty()) {
+//            System.out.println("Writer is null or empty");
+//            errors.rejectValue("writer", "trouble");
+//        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "writer", "writer is empty.");
         String sWriter = dto.getWriter();
-        if (sWriter == null || sWriter.trim().isEmpty()) {
-            System.out.println("Writer is null or empty");
-            errors.rejectValue("writer", "trouble");
+        if (sWriter.length() < 3) {
+            errors.rejectValue("writer", "writer is too short.");
         }
 
-        String sContent = dto.getContent();
-        if (sContent == null || sContent.trim().isEmpty()) {
-            System.out.println("Content is null or empty");
-            errors.rejectValue("content", "trouble");
-        }
+//        String sContent = dto.getContent();
+//        if (sContent == null || sContent.trim().isEmpty()) {
+//            System.out.println("Content is null or empty");
+//            errors.rejectValue("content", "trouble");
+//        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "content", "content is empty.");
     }
 }
--- src/main/java/com/study/helloworld/MyController.java
+++ src/main/java/com/study/helloworld/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
     @RequestMapping("/")
     // 메서드에 @ResponseBody 어노테이션이 있는 경우, 리턴되는 스트링값 자체만 웹브라우저로 전달
     public @ResponseBody String root() throws Exception {
-        return "Validator (1)";
+        return "Validator (2)";
     }
 
     // 단순히 jsp 페이지 리턴
@@ -33,6 +33,14 @@ public class MyController {
         ContentValidator validator = new ContentValidator();
         validator.validate(contentDto, result);
         if (result.hasErrors()) {
+            System.out.println("getAllErrors : " + result.getAllErrors());
+
+            if (result.getFieldError("writer") != null) {
+                System.out.println("1:" + result.getFieldError("writer").getCode());
+            }
+            if (result.getFieldError("content") != null) {
+                System.out.println("2:" + result.getFieldError("content").getCode());
+            }
             page = "createPage";
         }
 
