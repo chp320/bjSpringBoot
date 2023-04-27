4b1c269f8856c624f655dc08fdb6ec01afc97c40
chp320 <chp320@naver.com>
2023-04-28 00:07:50 +0900

    스프링의 유효성 검증 어노테이션(@NotNull, @NotEmpty)을 사용한 검증

--- src/main/java/com/study/helloworld/ContentDto.java
+++ src/main/java/com/study/helloworld/ContentDto.java
@@ -2,9 +2,18 @@ package com.study.helloworld;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class ContentDto {
     private int id;
+    @NotNull(message = "writer is null.")
+    @NotEmpty(message = "writer is empty.")
+    @Size(min = 3, max = 10, message = "writer min 3, max 10.")
     private String writer;
+    @NotNull(message = "content is null.")
+    @NotEmpty(message = "content is empty.")
     private String content;
 }
--- src/main/java/com/study/helloworld/MyController.java
+++ src/main/java/com/study/helloworld/MyController.java
@@ -18,7 +18,7 @@ public class MyController {
     @RequestMapping("/")
     // 메서드에 @ResponseBody 어노테이션이 있는 경우, 리턴되는 스트링값 자체만 웹브라우저로 전달
     public @ResponseBody String root() throws Exception {
-        return "Validator (3)";
+        return "Validator (4)";
     }
 
     // 단순히 jsp 페이지 리턴
@@ -28,30 +28,22 @@ public class MyController {
     }
 
     @RequestMapping("/create")
-//    public String insert2(@ModelAttribute("dto") ContentDto contentDto, BindingResult result) {
     public String insert2(@ModelAttribute("dto") @Valid ContentDto contentDto, BindingResult result) {
         String page = "createDonePage";
         System.out.println(contentDto);
 
-//        ContentValidator validator = new ContentValidator();
-//        validator.validate(contentDto, result);
         if (result.hasErrors()) {
-//            System.out.println("getAllErrors : " + result.getAllErrors());
-
             if (result.getFieldError("writer") != null) {
-                System.out.println("1:" + result.getFieldError("writer").getCode());
+                System.out.println("1:" + result.getFieldError("writer").getDefaultMessage());
             }
             if (result.getFieldError("content") != null) {
-                System.out.println("2:" + result.getFieldError("content").getCode());
+                System.out.println("2:" + result.getFieldError("content").getDefaultMessage());
             }
+
             page = "createPage";
         }
 
         return page;
     }
 
-    @InitBinder
-    protected void initBinder(WebDataBinder binder) {
-        binder.setValidator(new ContentValidator());
-    }
 }
