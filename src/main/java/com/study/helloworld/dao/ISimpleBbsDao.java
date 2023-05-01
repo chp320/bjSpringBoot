a7c6caf97a0a3c7522e9697736155a35082a742d
chp320 <chp320@naver.com>
2023-05-01 23:37:50 +0900

    17장.MyBatis에서 파라미터 사용하기 - @Param 어노테이션으로 mapper에서 사용할 이름 지정 가능. xml내 주석처리에 주의!!

--- src/main/java/com/study/helloworld/dao/ISimpleBbsDao.java
+++ src/main/java/com/study/helloworld/dao/ISimpleBbsDao.java
@@ -2,6 +2,7 @@ package com.study.helloworld.dao;
 
 import com.study.helloworld.dto.SimpleBbsDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -18,5 +19,7 @@ public interface ISimpleBbsDao {
     public int writeDao(String writer, String title, String content);
 
     /* delete Method */
-    public int deleteDao(String id);
+//    public int deleteDao(String id);
+    /* @Param 어노테이션 사용한 파라미터 추가 */
+    public int deleteDao(@Param("_id") String id); /* mapper에서도 파라미터명으로 지정한 값으로 수정 필요! */
 }
