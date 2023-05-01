a1efb5c64d407446a3f05f87d0d05351edc10e89
chp320 <chp320@naver.com>
2023-05-01 23:00:26 +0900

    16장.MyBatis로 간단한 게시판 만들기 - 데이터베이스 처리를 위한 DAO 인터페이스의 구현을 XML 파일로 하고, SQL 작성으로 실제 implements 수행

--- src/main/java/com/study/helloworld/MyController.java
+++ src/main/java/com/study/helloworld/MyController.java
@@ -1,10 +1,10 @@
 package com.study.helloworld;
 
 import com.study.helloworld.dao.ISimpleBbsDao;
-import com.study.helloworld.jdbc.IMyUserDao;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,23 +13,52 @@ import javax.servlet.http.HttpServletRequest;
 public class MyController {
 
     /* 생성자 주입 start */
-    private final IMyUserDao userDao;   // 인터페이스 타입의 객체 변수 -> mapper를 사용했기에 현 코드상에서는 implements한 클래스가 없음
+    private final ISimpleBbsDao dao;   // 인터페이스 타입의 객체 변수 -> mapper를 사용했기에 현 코드상에서는 implements한 클래스가 없음
 
-    public MyController(IMyUserDao userDao) {
-        this.userDao = userDao;
+    public MyController(ISimpleBbsDao dao) {
+        this.dao = dao;
     }
     /* 생성자 주입 end */
 
     @RequestMapping("/")
-    public @ResponseBody String root() throws Exception {
-        return "MyBatis 사용하기";
+    public String root() throws Exception {
+        // MyBatis: SimpleBBS
+        return "redirect:list";
     }
 
-    //    @GetMapping("/user")
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @RequestMapping("/list")
     public String userlistPage(Model model) {
-        model.addAttribute("users", userDao.list());
+        model.addAttribute("list", dao.listDao());
 
-        return "userlist";
+        return "list";
+    }
+
+    @RequestMapping("/view")
+    public String view(HttpServletRequest request, Model model) {
+        String sId = request.getParameter("id");    // queryString으로 전달되는 id값을 넘겨서 조회한다.
+        model.addAttribute("dto", dao.viewDao(sId));
+
+        return "view";
+    }
+
+    @RequestMapping("/writeForm")
+    public String writeForm() {
+
+        return "writeForm";
+    }
+
+    @RequestMapping("/write")
+    public String write(HttpServletRequest request, Model model) {
+        dao.writeDao(request.getParameter("writer"),
+                request.getParameter("title"),
+                request.getParameter("content"));
+        return "redirect:list";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(HttpServletRequest request, Model model) {
+        dao.deleteDao(request.getParameter("id"));
+
+        return "redirect:list";
     }
 }
--- src/main/java/com/study/helloworld/dao/ISimpleBbsDao.java
+++ src/main/java/com/study/helloworld/dao/ISimpleBbsDao.java
@@ -1,9 +1,11 @@
 package com.study.helloworld.dao;
 
 import com.study.helloworld.dto.SimpleBbsDto;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper // 다음 인터페이스의 구현을 XML로 한다는 의미.
 public interface ISimpleBbsDao {
 
     /* select Method */
