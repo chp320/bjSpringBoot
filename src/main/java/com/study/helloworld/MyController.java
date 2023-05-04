077d5a6fb882186804060f4ad4526302ac079f4f
chp320 <chp320@naver.com>
2023-05-05 01:14:33 +0900

    25장.시큐리티 기초 - WebSecurityConfigurerAdapter를 상속한 클래스를 구현한 시큐리티 설정 클래스로 간단한 로그인 기능 및 메뉴별 권한 설정 구현

--- src/main/java/com/study/helloworld/MyController.java
+++ src/main/java/com/study/helloworld/MyController.java
@@ -1,44 +1,29 @@
 package com.study.helloworld;
 
-import com.study.helloworld.service.BuyAndLogService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
-    /* 컨트롤러에서 호출하는 서비스를 IBuyTicketService -> BuyAndLogService 로 변경 */
-    @Autowired
-    BuyAndLogService buyAndLog;
 
     @RequestMapping("/")
     public @ResponseBody String root() throws Exception {
-        return "Transaction X (4)";
+        return "Security (1)";
     }
 
-    @RequestMapping("/buy_ticket")
-    public String buy_ticket() {
-        return "buy_ticket";
+    @RequestMapping("/guest/welcome")
+    public String welcome1() {
+        return "guest/welcome1";
     }
 
-    @RequestMapping("/buy_ticket_card")
-    public String buy_ticket_card(@RequestParam("consumerId") String consumerId,
-                                  @RequestParam("amount") String amount,
-                                  @RequestParam("error") String error,
-                                  Model model) {
-        int nResult = buyAndLog.buy(consumerId, Integer.parseInt(amount), error);
-        System.out.println("nResult: " + nResult);
+    @RequestMapping("/member/welcome")
+    public String welcome2() {
+        return "member/welcome2";
+    }
 
-        model.addAttribute("consumerId", consumerId);
-        model.addAttribute("amount", amount);
-
-        if (nResult == 1) {
-            return "buy_ticket_end";
-        } else {
-            return "buy_ticket_error";
-        }
+    @RequestMapping("/admin/welcome")
+    public String welcome3() {
+        return "admin/welcome3";
     }
 }
