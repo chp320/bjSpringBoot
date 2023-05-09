f41b4f4e9f08dc2a829b2d22467bd180e719c83a
chp320 <chp320@naver.com>
2023-05-10 00:02:43 +0900

    29장.시큐리티 데이터베이스 사용하기 - DB 테이블에 저장된 암호화된 패스워드를 조회하여 화면에서 입력된 plain 패스워드를 비교해서 로그인 처리하는 기능 구현

--- src/main/java/com/study/helloworld/auth/WebSecurityConfig.java
+++ src/main/java/com/study/helloworld/auth/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+import javax.sql.DataSource;
+
 @Configuration          // 스프링 설정으로 사용한다는 의미
 @EnableWebSecurity      // 스프링 시큐리티 기능 활성화 의미
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -56,6 +58,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
     }
 
+    // DB 테이블에서 회원 정보 조회해서 사용하는 기능 구현을 위해 아래 하드코딩 부분 주석 처리
+    /*
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -63,6 +67,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN");
         // ROLE_ADMIN 에서 ROLE_ 는 자동으로 붙는다.
+    }
+    */
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        System.out.println(passwordEncoder().encode("123"));    // 인코딩 테스트 코드. 콘솔에 출력된 값으로 테이블에 update한다.
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)     // DB 접속 정보를 이용
+                .usersByUsernameQuery("select name as userName, password, enabled from user_list where name = ?")
+                .authoritiesByUsernameQuery("select name as userName, authority from user_list where name = ?")
+                .passwordEncoder(new BCryptPasswordEncoder());  // 입력한 비밀번호를 DB 암호화된 패스워드와 비교해서 올바른 값인지 여부 확인
     }
 
     // passwordEncoder() 추가
